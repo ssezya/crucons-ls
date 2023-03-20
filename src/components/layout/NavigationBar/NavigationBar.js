90d5c6017caa70617bede7592e6ab2faa46b0396
Sergey Golubtsov <ssezya@yandex.ru>
2023-03-20 12:33:04 +0300

    Added UserDropdown component

--- src/components/layout/NavigationBar/NavigationBar.js
+++ src/components/layout/NavigationBar/NavigationBar.js
@@ -1,17 +1,16 @@
-import { useDispatch } from 'react-redux';
 import { Layout } from 'antd';
 
-import { logout } from '@store/auth/auth.actions';
+import { UserDropdown } from './UserDropdown/UserDropdown';
 
 import './NavigationBar.styles.scss';
 
 export const NavigationBar = () => {
-    const dispatch = useDispatch();
-    const handleLogout = () => dispatch(logout());
-
     return (
         <Layout.Header className='navigation-bar'>
             <div className='navigation-bar-logo'></div>
+            <div className='navigation-bar-user'>
+                <UserDropdown />
+            </div>
         </Layout.Header>
     );
 };
---
+++ src/components/layout/NavigationBar/UserDropdown/UserDropdown.js
@@ -0,0 +1,37 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { Dropdown, Avatar } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
+
+import { logout } from '@store/auth/auth.actions';
+
+import './UserDropdown.styles.scss';
+
+export const UserDropdown = () => {
+    const userFullName = useSelector(state => state.user.info.fullName);
+    const dispatch = useDispatch();
+    const handleLogout = () => dispatch(logout());
+
+    const items = [
+        {
+            key: 1,
+            disabled: true,
+            label: userFullName ?? 'User profile'
+        },
+        {
+            key: 2,
+            danger: true,
+            label: <a rel='noopener noreferrer' href='/login' onClick={handleLogout}>Logout</a>
+        }
+    ];
+
+    return (
+        <Dropdown
+            className='user-dropdown'
+            menu={{ items }}
+            trigger='click'
+            placement='bottomRight'
+        >
+            <Avatar className='user-dropdown-avatar' shape='square' icon={<UserOutlined />} />
+        </Dropdown>
+    );
+};
